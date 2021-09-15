d633c72ab896877b5c49178fa0891b40a5f3f00f
IbrahimAtef22 <82728944+IbrahimAtef22@users.noreply.github.com>
2021-09-15 07:36:15 +0200

    Add files via upload

---
+++ task8.php
@@ -0,0 +1,41 @@
+<?php
+
+
+//hospital management system
+/*
+
+admins
+id    name   email    password
+
+==============================================================================
+
+doctors
+id     name     email      password      specialize    gender    
+
+
+
+==============================================================================
+docAppointments
+doctorID   patientID   day   month   year    time    reservation     check
+  1           1        3       9     2021    3 pm       yes         accept or not
+  2           2 
+  3           3
+==============================================================================
+
+Patients
+id     name    email      password   
+
+
+===============================================================================
+
+doctor      patient
+1             m
+m             1
+=================
+m      :       m
+
+
+
+*/
+
+?>
